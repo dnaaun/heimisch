f36903df97b89d1cf2fad18c249546e228713c45
davidatsurge <93471693+davidatsurge@users.noreply.github.com>
2024-12-21 12:18:36 +0300

    chore: upgrade wasm-bindgen (cuz cargo-leptos uses a later version)

---
+++ web/src/app/issues_tab/list.rs
@@ -0,0 +1,9 @@
+use leptos::prelude::*;
+
+#[component]
+pub fn IssuesList() -> impl IntoView {}
+
+#[component]
+pub fn MyRouter(#[prop(optional)] prefix: Option<&'static str>,
+    ) -> impl IntoView {
+}
