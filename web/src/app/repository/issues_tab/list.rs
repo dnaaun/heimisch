a769d06b01dc69f9164ef995b223a247e6af61ac
davidatsurge <93471693+davidatsurge@users.noreply.github.com>
2024-12-24 09:21:02 +0300

    .

--- web/src/app/repository/issues_tab/list.rs
+++ web/src/app/repository/issues_tab/list.rs
@@ -1,9 +1,5 @@
 use leptos::prelude::*;
 
 #[component]
-pub fn IssuesList() -> impl IntoView {}
-
-#[component]
-pub fn MyRouter(#[prop(optional)] prefix: Option<&'static str>,
-    ) -> impl IntoView {
+pub fn IssuesList() -> impl IntoView {
 }
--- web/src/app/repository/issues_tab/mod.rs
+++ web/src/app/repository/issues_tab/mod.rs
@@ -1,3 +1,5 @@
+pub mod list;
+
 use std::ops::Deref;
 
 use itertools::Itertools;
