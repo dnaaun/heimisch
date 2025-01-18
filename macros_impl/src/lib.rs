3d219d4573df6b5b16f809dbd582ee5487f11b0f
davidatsurge <93471693+davidatsurge@users.noreply.github.com>
2025-01-18 20:51:20 +0300

    rename typed_router to zwang_router

--- macros_impl/src/lib.rs
+++ macros_impl/src/lib.rs
@@ -4,9 +4,9 @@
 extern crate proc_macro;
 
 mod avail_merge;
-mod typed_router;
+mod zwang_router;
 mod typesafe_idb;
 
 pub use avail_merge::derive_avail_merge;
 pub use typesafe_idb::derive_typesafe_idb;
-pub use typed_router::zwang_routes;
+pub use zwang_router::zwang_routes;
